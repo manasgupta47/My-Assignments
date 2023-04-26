bb3dfeee14089caa913a54beb7d13a88898a026e
manasgupta47 <68424022+manasgupta47@users.noreply.github.com>
2023-04-26 13:43:02 +0530

    Update Q1.java

--- Basic Data Structure Assignment/Q1.java
+++ Basic Data Structure Assignment/Q1.java
@@ -1,1 +1,33 @@
 
+import java.util.Scanner;
+
+class ArmstrongOrNot {
+public boolean armstrongCheck(int num){
+    boolean ans=false;
+    int sum=0;
+    int temp=num;
+    while(num!=0){
+        int n=num%10;
+        sum += n*n*n;
+        num=num/10;
+    }
+    if(sum==temp){
+        ans=true;
+    }
+    return ans;
+}
+}
+public class Assignment1Q1 {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        System.out.println("Enter the number");
+        int num=sc.nextInt();
+        ArmstrongOrNot ATN=new ArmstrongOrNot();
+        if(ATN.armstrongCheck(num)) {
+            System.out.println(num+" is armstrong number");
+        }
+        else{
+            System.out.println(num+" is not 4armstrong number");
+        }
+    }
+}
