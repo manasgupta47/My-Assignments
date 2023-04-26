ac09695880b1de78e10d74f9de188795087f8d42
manasgupta47 <68424022+manasgupta47@users.noreply.github.com>
2023-04-26 17:48:45 +0530

    Update Q1.java

--- Intermediate Opps Assignment/Q1.java
+++ Intermediate Opps Assignment/Q1.java
@@ -1,1 +1,19 @@
 
+class SingletonInheritanceCheck {
+    private static SingletonInheritanceCheck instance;
+    private SingletonInheritanceCheck(){}
+    public static SingletonInheritanceCheck getInstance(){
+        if(instance==null)
+            instance=new SingletonInheritanceCheck();
+        return instance;
+    }
+}
+public class MySingleton extends SingletonInheritanceCheck{
+    //This will not complie we got an error saying that
+    // Signleton costructor is not visiable
+}
+public class Assignment2Q1 {
+public static void main(String args[]){
+SingletonInheritanceCheck obj1=SingletonInheritanceCheck.getInstance();
+  }
+}
