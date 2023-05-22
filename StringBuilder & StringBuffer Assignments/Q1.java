58e2bfa409d0755eb8afb0651a34ca89ca3657fc
manasgupta47 <68424022+manasgupta47@users.noreply.github.com>
2023-05-22 10:36:06 +0530

    Create Q1.java

---
+++ StringBuilder & StringBuffer Assignments/Q1.java
@@ -0,0 +1,6 @@
+public class lengthOfString {
+    public static void main(String[] args) {
+        String str="Hello World";
+        System.out.println("Length of String "+str.length());
+    }
+}
