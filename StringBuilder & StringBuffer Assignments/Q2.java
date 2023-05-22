a6cb9b8d0e55f0bd868177c48def8176820064f5
manasgupta47 <68424022+manasgupta47@users.noreply.github.com>
2023-05-22 10:38:55 +0530

    Create Q2.java

---
+++ StringBuilder & StringBuffer Assignments/Q2.java
@@ -0,0 +1,7 @@
+public class JoinTheString {
+    public static void main(String[] args) {
+        String s1="Hello ";
+        String s2="how are you?";
+        System.out.println(s1.concat(s2));
+    }
+}
